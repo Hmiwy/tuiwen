a85ba6ddd2f9f251db9ee96cc3c8101b0d3de5c0
Hmiwy <53623487+Hmiwy@users.noreply.github.com>
2024-05-06 12:12:07 +0800

    Update tureen.js

--- tureen.js
+++ tureen.js
@@ -3,47 +3,33 @@
 *******************************
 
 [rewrite_local]
-
-^http[s]?:\/\/league-api.huazhuanapp.com\/api\/v1\/user\/permanentPoints? url script-response-body https://raw.githubusercontent.com/Hmiwy/tuiwen/main/tureen.js
-^http[s]?:\/\/league-api.huazhuanapp.com\/api\/v1\/wallpaper\/generator\/vip\/info url script-response-body https://raw.githubusercontent.com/SNKrock/3430460/main/twzs.js
-
+^http:\/\/api\.tuiwenzhushou\.top\/user\/ url script-response-body 
 [mitm] 
 
-hostname = league-api.huazhuanapp.com
-
+hostname = api.tuiwenzhushou.top
 *******************************/
-/*const body = $response.body.replace(/"vipPoints":\d+/g,'"vipPoints":999999999')
-.replace(/"permanentPoint":\d+/g,'"permanentPoint":999999999')
-.replace(/"showVipAlert":\w+/g,'"showVipAlert":true')
-.replace(/"vipDesc":".*?"/g,'"vipDesc":"会员有效期至：2999-09-09 19:19"')
-.replace(/"mobile":".*?"/g,'"mobile":"139****8888(年卡会员)"')
-.replace(/"coins":\d+/g,'"coins":99999999999')
-.replace(/"vipDesc":".*?"/g,'"vipDesc":"会员有效期至：2099-09-09 14:38"')
-.replace(/"mobile":".*?"/g,'"mobile":"139****9999(年卡会员)"')
-.replace(/"coins":\d+/g,'"coins":99999999')
-.replace(/"showVipAlert":\w+/g,'"showVipAlert":true')
-.replace(/"vipStatus":\d/g,'"vipStatus":1')
+                               
+/*{
+  "data" : {
+    "id" : 5254469,
+    "app_id" : 50,
+    "add_time" : 1714967952,
+    "access_time" : 1714967952,
+    "status" : 1,
+    "open_id" : "",
+    "reg_ip" : "171.106.113.65",
+    "type" : 0,
+    "vip_type" : 0,
+    "reg_pay" : 0,
+    "is_vip" : 0,
+    "vip_end_time" : 0,
+    "skey" : "7f89da69a7e5786f488af8396db2b70b"
+  },
+  "code" : 0
+}
+(/"vipPoints":\d+/g,'"vipPoints":999999999')
+*/
+var body = $response.body.replace(/"is_vip":\d+/g,'"is_vip":1')
+.replace(/"vip_end_time":\d+/g,'"vip_end_time":2099-12-12')
 $done({ body });
-/* "vipDetailDesc" : "每日24点重置",
-  "permanentPointDesc" : "永久积分长期有效，不使用不会消耗，不会重置清零",
-  "price" : 10,
-  "vipPoints" : 0,
-  "coins" : 0,
-  "permanentPoint" : 0,
-  "vipPointDesc" : "会员每日赠送会员积分，每日24点重置"*/
 
-
-var body = $response.body.replace(/"price":\d+/g,'"price":0') 
-  .replace(/"vipPoints":\d+/g,'"vipPoints":999999999')
- .replace(/"permanentPoint":\d+/g,'"permanentPoint":9999')
-.replace(/"showVipAlert":\w+/g,'"showVipAlert":true')
-.replace(/"vipDesc":".*?"/g,'"vipDesc":"会员有效期至：2999-09-09 19:19"')
-.replace(/"mobile":".*?"/g,'"mobile":"139****8888(年卡会员)"')
-.replace(/"coins":\d+/g,'"coins":99999999999')
-.replace(/"vipDesc":".*?"/g,'"vipDesc":"会员有效期至：2099-09-09 14:38"')
-.replace(/"mobile":".*?"/g,'"mobile":"139****9999(年卡会员)"')
-.replace(/"coins":\d+/g,'"coins":99999999')
-.replace(/"showVipAlert":\w+/g,'"showVipAlert":true')
-.replace(/"vipStatus":\d/g,'"vipStatus":1')
-.repalce(/"currentMoney":\d+/g,'"currentMoney":9999')
-  $done({ body });                                 
